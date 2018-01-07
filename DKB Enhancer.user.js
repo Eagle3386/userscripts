eeb2d269f53e9766d9714d437f624b4b4d5eb384
Martin Baranski <5111490+Eagle3386@users.noreply.github.com>
2018-01-07 12:56:37 +0100

    Fixed document.getElementById() returns NULL

--- DKB Enhancer.user.js
+++ DKB Enhancer.user.js
@@ -3,9 +3,9 @@
 // @namespace   https://www.dkb.de/banking/finanzstatus/kontoumsaetze
 // @include     https://www.dkb.de/banking/finanzstatus/kontoumsaetze*
 // @icon        https://www.dkb.de/favicon.ico
-// @run-at      document-start
+// @run-at      document-end
 // @description Always shows daily balance diagram
-// @version     1.1
+// @version     1.2
 // @author      Martin Baranski
 // @homepageURL https://github.com/Eagle3386/userscripts
 // @license     GPL v2.0
