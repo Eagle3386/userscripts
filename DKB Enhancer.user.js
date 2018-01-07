f197a6626ed26f6209b2082cf4967e2dba288626
Martin Baranski <5111490+Eagle3386@users.noreply.github.com>
2018-01-07 12:55:34 +0100

    Updated diagram display enforcement (pure JS, no GM_addStyle needed anymore)

--- DKB Enhancer.user.js
+++ DKB Enhancer.user.js
@@ -5,15 +5,16 @@
 // @icon        https://www.dkb.de/favicon.ico
 // @run-at      document-start
 // @description Always shows daily balance diagram
-// @version     1.0
+// @version     1.1
 // @author      Martin Baranski
 // @homepageURL https://github.com/Eagle3386/userscripts
 // @license     GPL v2.0
-// @grant       GM_addStyle
+// @grant       none
 // ==/UserScript==
 
 (function() {
   'use strict';
 
-  GM_addStyle("#dailyBalanceDiagram { display: block !important; }");
+  document.getElementById('dailyBalanceDiagram').style.setProperty('display', 'block', 'important');
+  document.getElementById('iconExpandDiagram').classList.replace('iconExpandMore', 'iconExpandLess');
 })();
