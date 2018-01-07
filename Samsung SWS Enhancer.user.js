077df9485a09b8947f3887ab576fb337de649133
Martin Baranski <5111490+Eagle3386@users.noreply.github.com>
2018-01-07 15:48:16 +0100

    Updated SWS Enhancer (pure JS, no GM_addStyle needed anymore)

--- Samsung SWS Enhancer.user.js
+++ Samsung SWS Enhancer.user.js
@@ -5,26 +5,29 @@
 // @icon        https://lh3.googleusercontent.com/gUukYHEJnsm8r3P3XkVOOKZ4Mb7HwyG1ixaq4h7Rsd7s9AgrMuDsq3OBpKrvJyvYIA
 // @run-at      document-start
 // @description Fixes improper height/width values of toner/paper tray panel
-// @version     1.2
+// @version     1.3
 // @author      Martin Baranski
 // @homepageURL https://github.com/Eagle3386/userscripts
 // @license     GPL v2.0
-// @grant       GM_addStyle
+// @grant       none
 // ==/UserScript==
 
 (function() {
   'use strict';
 
+  const style = document.createElement('style');
+  style.type = 'text/css';
   // Panel heights
-  GM_addStyle("#ext-gen332, #ext-gen376 { height: 124px !important; }");
-  GM_addStyle("#ext-gen428 { height: 73px !important; }");
+  style.appendChild(document.createTextNode('#ext-gen332, #ext-gen376 { height: 124px !important; }'));
+  style.appendChild(document.createTextNode('#ext-gen428 { height: 73px !important; }'));
 
   // Toner panel - first/last column
-  GM_addStyle("#ext-gen355 .x-grid3-td-name, #ext-gen350 .x-grid3-td-name, #ext-gen355 .x-grid3-td-2, #ext-gen350 .x-grid3-td-2 { width: 95px !important; }");
+  style.appendChild(document.createTextNode('#ext-gen355 .x-grid3-td-name, #ext-gen350 .x-grid3-td-name, #ext-gen355 .x-grid3-td-2, #ext-gen350 .x-grid3-td-2 { width: 95px !important; }'));
 
   // Paper tray panel columns
-  GM_addStyle("#ext-gen399 .x-grid3-td-name, #ext-gen394 .x-grid3-td-name { width: 135px !important; }");
-  GM_addStyle("#ext-gen399 .x-grid3-td-1, #ext-gen394 .x-grid3-td-1 { width: 95px !important; }");
-  GM_addStyle("#ext-gen399 .x-grid3-td-2, #ext-gen394 .x-grid3-td-2 { width: 100px !important; }");
-  GM_addStyle("#ext-gen399 .x-grid3-td-3, #ext-gen394 .x-grid3-td-3 { width: 80px !important; }");
+  style.appendChild(document.createTextNode('#ext-gen399 .x-grid3-td-name, #ext-gen394 .x-grid3-td-name { width: 135px !important; }'));
+  style.appendChild(document.createTextNode('#ext-gen399 .x-grid3-td-1, #ext-gen394 .x-grid3-td-1 { width: 95px !important; }'));
+  style.appendChild(document.createTextNode('#ext-gen399 .x-grid3-td-2, #ext-gen394 .x-grid3-td-2 { width: 100px !important; }'));
+  style.appendChild(document.createTextNode('#ext-gen399 .x-grid3-td-3, #ext-gen394 .x-grid3-td-3 { width: 80px !important; }'));
+  document.getElementsByTagName('head')[0].appendChild(style);
 })();
