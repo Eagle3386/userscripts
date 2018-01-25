acea170ca162bd58e321ea66ba6dfa707a1abaf5
Martin Baranski <5111490+Eagle3386@users.noreply.github.com>
2018-01-25 11:31:42 +0100

    Updated to use strict mode

--- Microsoft EN-US Enforcer.user.js
+++ Microsoft EN-US Enforcer.user.js
@@ -5,14 +5,18 @@
 // @icon        https://www.microsoft.com/favicon.ico
 // @run-at      document-start
 // @description Redirect to US-English versions of Microsoft websites, except when non-EN-US is enforced.
-// @version     2.1
+// @version     2.2
 // @author      RoundRobin (original), Martin Baranski (improvement)
 // @homepageURL https://github.com/Eagle3386/userscripts
 // @license     GPL v2.0
 // ==/UserScript==
 
-if (/\/(\w{2}-\w{2})\//.test(window.location.pathname)
-    && !window.location.pathname.startsWith('/en-us')
-    && window.location.search.indexOf("language=force") == -1) {
-  window.location.pathname = window.location.pathname.replace(/\/(\w{2}-\w{2})\//, "/en-us/");
-}
+(function() {
+  'use strict';
+
+  if (/\/(\w{2}-\w{2})\//.test(window.location.pathname)
+      && !window.location.pathname.startsWith('/en-us')
+      && window.location.search.indexOf('language=force') == -1) {
+    window.location.pathname = window.location.pathname.replace(/\/(\w{2}-\w{2})\//, '/en-us/');
+  }
+})();
