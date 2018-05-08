e00a3b1cc46b2665381b5116d77c0318c2481270
Martin Baranski <5111490+Eagle3386@users.noreply.github.com>
2018-05-08 09:17:02 +0200

    Updated GCalendar Colored Weekends
    - fixes mini month navigator (Fri/Sat instead of Sat/Sun emphasized)
    - removes unavailable CSS selector
    - improves script's name

--- GCalendar Colored Weekends.user.js
+++ GCalendar Colored Weekends.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name        Google Calendar Colored Weekends (Fall 2017)
+// @name        Google Calendar Colored Weekends
 // @namespace   https://calendar.google.com
 // @include     https://calendar.google.com/*
 // @icon        https://calendar.google.com/googlecalendar/images/favicon_v2014_31.ico
 // @run-at      document-start
 // @description Emphasizes background of weekends in monthly view.
-// @version     2.1
+// @version     2.2
 // @author      Martin Baranski
 // @homepageURL https://github.com/Eagle3386/userscripts
 // @license     EUPL, Version 1.2 only
@@ -17,8 +17,8 @@
 
   const style = document.createElement('style');
   style.type = 'text/css';
-  style.appendChild(document.createTextNode('.elYzab-cXXICe-Hjleke:nth-child(6) { background-color: #F4F4F4; }'));
-  style.appendChild(document.createTextNode('.elYzab-cXXICe-Hjleke:nth-child(7) { background-color: #EDEDED; }'));
-  style.appendChild(document.createTextNode('.ecKr0, .ef2wWc { background-color: #DB4437 !important; }')); // Today
+  style.appendChild(document.createTextNode('div.elYzab-cXXICe-Hjleke:nth-child(6), span.elYzab-DaY83b-ppHlrf:nth-child(7) { background-color: #F4F4F4; }'));
+  style.appendChild(document.createTextNode('div.elYzab-cXXICe-Hjleke:nth-child(7), span.elYzab-DaY83b-ppHlrf:nth-child(8) { background-color: #EDEDED; }'));
+  style.appendChild(document.createTextNode('.ef2wWc { background-color: #DB4437 !important; }'));
   document.getElementsByTagName('head')[0].appendChild(style);
 })();
