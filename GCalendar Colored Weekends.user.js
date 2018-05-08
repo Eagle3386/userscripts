0fc4519ea1a012e0fda093480904d8a8ae100cd3
Martin Baranski <5111490+Eagle3386@users.noreply.github.com>
2018-05-08 11:31:05 +0200

    Update GCalendar Colored Weekends.user.js

--- GCalendar Colored Weekends.user.js
+++ GCalendar Colored Weekends.user.js
@@ -5,7 +5,7 @@
 // @icon        https://calendar.google.com/googlecalendar/images/favicon_v2014_31.ico
 // @run-at      document-start
 // @description Emphasizes backgrounds of today and weekends in (mini) monthly view.
-// @version     2.2
+// @version     2.3
 // @author      Martin Baranski
 // @homepageURL https://github.com/Eagle3386/userscripts
 // @license     EUPL, Version 1.2 only
