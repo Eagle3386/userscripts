9e4421a2527bcd7926d482d071b9fea949d30ab5
Martin Arndt <5111490+Eagle3386@users.noreply.github.com>
2018-10-07 19:28:21 +0200

    Fix @include URL

--- FB Enhancer.user.js
+++ FB Enhancer.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name        Facebook Enhancer
 // @namespace   https://www.facebook.com/checkpoint
-// @include     https://www.facebook.com/checkpoint/?next
+// @include     https://www.facebook.com/checkpoint/?next*
 // @icon        https://www.facebook.com/favicon.ico
 // @run-at      document-end
 // @description Always checks "Don't save" to *not* skip 2FA upon login
-// @version     1.0
+// @version     1.1
 // @author      Martin Baranski
 // @homepageURL https://github.com/Eagle3386/userscripts
 // @license     EUPL, Version 1.2 only
