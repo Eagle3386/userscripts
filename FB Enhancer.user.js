c7dc8854ac95c75d458168f805c2ba9d4c71dfca
Martin Arndt <5111490+Eagle3386@users.noreply.github.com>
2018-10-08 10:52:47 +0200

    Updated name

--- FB Enhancer.user.js
+++ FB Enhancer.user.js
@@ -6,7 +6,7 @@
 // @run-at      document-end
 // @description Always checks "Don't save" to *not* skip 2FA upon login
 // @version     1.2
-// @author      Martin Baranski
+// @author      Martin Arndt
 // @homepageURL https://github.com/Eagle3386/userscripts
 // @license     EUPL, Version 1.2 only
 // @grant       none
