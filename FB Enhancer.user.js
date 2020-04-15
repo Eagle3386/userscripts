156b6d1cb8649c131cf03b356ad7b739bdaacc11
Martin Arndt <5111490+Eagle3386@users.noreply.github.com>
2020-04-15 12:31:26 +0200

    Fix icon URL V2

--- FB Enhancer.user.js
+++ FB Enhancer.user.js
@@ -2,10 +2,10 @@
 // @name        Facebook Enhancer
 // @namespace   https://www.facebook.com/checkpoint
 // @include     https://www.facebook.com/checkpoint/?next*
-// @icon        https://static.xx.fbcdn.net/rsrc.php/yo/r/iRmz9lCMBD2.ico
+// @icon        https://external-content.duckduckgo.com/ip3/www.facebook.com.ico
 // @run-at      document-end
 // @description Always checks "Don't save" to *not* skip 2FA upon login
-// @version     1.2
+// @version     1.3
 // @author      Martin Arndt
 // @homepageURL https://github.com/Eagle3386/userscripts
 // @license     EUPL, Version 1.2 only
