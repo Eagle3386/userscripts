1ed8545c78ae6fc5766ba9d75b679efd37ede911
Martin Arndt <5111490+Eagle3386@users.noreply.github.com>
2020-10-14 12:54:35 +0200

    Adapt to recent GMail CSS changes

--- GMail Custom Fixed Font.user.js
+++ GMail Custom Fixed Font.user.js
@@ -5,7 +5,7 @@
 // @icon        https://ssl.gstatic.com/ui/v1/icons/mail/favicon.ico
 // @run-at      document-start
 // @description Custom fixed-font in GMail messages
-// @version     1.6
+// @version     1.7
 // @author      Marcin Rataj (original), Martin Arndt (tweaks)
 // @homepageURL https://github.com/Eagle3386/userscripts
 // @license     CC0; https://creativecommons.org/publicdomain/zero/1.0/
@@ -23,5 +23,5 @@ head.appendChild(link);
 
 const style = document.createElement('style');
 style.type = 'text/css';
-style.appendChild(document.createTextNode('.ii, .Ak, .editable { font-family: \'' + font + '\', monospace !important; }'));
+style.appendChild(document.createTextNode('.editable { font-family: \'' + font + '\', monospace !important; }'));
 head.appendChild(style);
