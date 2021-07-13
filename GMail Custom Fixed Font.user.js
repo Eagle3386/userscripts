ac5040e50547816a89c2f39b3d0ff32e9c34c6f0
Martin Arndt <5111490+Eagle3386@users.noreply.github.com>
2021-07-13 09:29:34 +0200

    Update desired font's name

--- GMail Custom Fixed Font.user.js
+++ GMail Custom Fixed Font.user.js
@@ -5,14 +5,14 @@
 // @icon        https://ssl.gstatic.com/ui/v1/icons/mail/favicon.ico
 // @run-at      document-start
 // @description Custom fixed-font in GMail messages
-// @version     1.7
+// @version     1.8
 // @author      Marcin Rataj (original), Martin Arndt (tweaks)
 // @homepageURL https://github.com/Eagle3386/userscripts
 // @license     CC0; https://creativecommons.org/publicdomain/zero/1.0/
 // @grant       none
 // ==/UserScript==
 
-const font = 'Source Code Pro';
+const font = 'Source Code Variable';
 
 // Dragons below this line ;-)
 const head = document.getElementsByTagName('head')[0];
