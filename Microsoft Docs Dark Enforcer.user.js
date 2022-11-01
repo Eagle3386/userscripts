cfe291042cd4f8e17943a22751677e9fea9cdc4d
Martin Arndt <5111490+Eagle3386@users.noreply.github.com>
2022-11-01 17:04:29 +0100

    Add support for `learn` subdomain

--- Microsoft Docs Dark Enforcer.user.js
+++ Microsoft Docs Dark Enforcer.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name        Microsoft Docs Dark Enforcer
-// @namespace   /^https?://docs\.microsoft\.com/\w{2}-\w{2}/$
-// @include     /^https?://docs\.microsoft\.com/\w{2}-\w{2}/.*$
+// @namespace   /^https?://(docs|learn)\.microsoft\.com/\w{2}-\w{2}/$
+// @include     /^https?://(docs|learn)\.microsoft\.com/\w{2}-\w{2}/.*$
 // @icon        https://www.microsoft.com/favicon.ico
 // @run-at      document-start
 // @description Enforce dark theme, even when reset by browser privacy add-ons clearing local/session storage.
-// @version     1.2
+// @version     1.3
 // @author      Martin Arndt
 // @homepageURL https://github.com/Eagle3386/userscripts
 // @license     EUPL, Version 1.2 only
