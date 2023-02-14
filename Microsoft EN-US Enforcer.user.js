ea4aa1ffc2ff4c20ccbf101e4e8b6ecb96462e1b
Martin Arndt <5111490+Eagle3386@users.noreply.github.com>
2023-02-14 13:50:23 +0100

    Fix redirect loop upon later lang-code location

--- Microsoft EN-US Enforcer.user.js
+++ Microsoft EN-US Enforcer.user.js
@@ -5,7 +5,7 @@
 // @icon        https://www.microsoft.com/favicon.ico
 // @run-at      document-start
 // @description Redirect to US-English versions of Microsoft websites, except when non-EN-US is enforced.
-// @version     2.4
+// @version     2.5
 // @author      RoundRobin (original), Martin Arndt (improvement)
 // @homepageURL https://github.com/Eagle3386/userscripts
 // @license     EUPL, Version 1.2 only
@@ -16,8 +16,8 @@
   'use strict';
 
   if (/\/(\w{2}-\w{2})\//.test(window.location.pathname)
-      && !window.location.pathname.startsWith('/en-us')
-      && window.location.search.indexOf('language=force') == -1) {
+      && window.location.pathname.indexOf('/en-us/') == -1
+      && window.location.search.indexOf('lang=force') == -1) {
     window.location.pathname = window.location.pathname.replace(/\/(\w{2}-\w{2})\//, '/en-us/');
   }
 })();
